7a060e732818f1cd5b79879790f6e04e5de19a49
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-30 12:40:26 -0400

    Made the Navigation the parent component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/Home";
+import Navigation from "./routes/navigation/Navigation";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
