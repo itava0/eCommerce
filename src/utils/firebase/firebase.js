1876ebfba8e81e4aebaa87aed3bbb9834a3e7212
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-04 18:15:10 -0400

    Created a method that create a new user.

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -4,6 +4,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -58,4 +59,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   }
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
