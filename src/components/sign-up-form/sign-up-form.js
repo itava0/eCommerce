6bc6aac5e6454b4c7aa2687cb70709573bd7c376
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-05 21:30:24 -0400

    Keeping track of the current user with context

--- src/components/sign-up-form/sign-up-form.js
+++ src/components/sign-up-form/sign-up-form.js
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import FormInput from '../form-input/form-input';
 import Button from '../button/button';
+
+import { UserContext } from '../../contexts/user.context';
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -19,6 +21,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -36,6 +40,8 @@ const SignUpForm = () => {
         email,
         password
       );
+
+      setCurrentUser(user);
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
--- src/routes/navigation/Navigation.js
+++ src/routes/navigation/Navigation.js
@@ -1,22 +1,36 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import './navigation.scss'
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+
+import { signOutUser } from "../../utils/firebase/firebase";
+import "./navigation.scss";
 
 const Navigation = () => {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null)
+  }
+
   return (
     <Fragment>
       <nav className="navigation">
-        <Link className="logo-container" to='/'>
-          <CrwnLogo className="logo"/>
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/sign-in">
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}> SIGN OUT</span>
+          ) : (
+            <Link className="nav-link" to="/sign-in">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </nav>
       <Outlet />
