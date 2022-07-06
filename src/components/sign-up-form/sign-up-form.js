0a18a31b0306cb9268b419e63de29d7a2dbb5f7d
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-06 14:36:49 -0400

    Made some chages to keep track of the user

--- src/components/sign-up-form/sign-up-form.js
+++ src/components/sign-up-form/sign-up-form.js
@@ -1,8 +1,6 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import FormInput from '../form-input/form-input';
 import Button from '../button/button';
-
-import { UserContext } from '../../contexts/user.context';
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -21,8 +19,6 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -40,8 +36,6 @@ const SignUpForm = () => {
         email,
         password
       );
-
-      setCurrentUser(user);
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
