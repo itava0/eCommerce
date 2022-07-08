bc2a4c6885c7c4bff67ec59800c085afa79a7a6d
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-07-07 21:32:19 -0400

    Shop Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/Home";
 import Navigation from "./routes/navigation/Navigation";
+import Shop from "./routes/shop/shop";
 import Authentication from "./routes/authentication/authentication";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="sign-in" element={<Authentication />} />
       </Route>
     </Routes>
